fe1645239e2e19ff90e9178d3ae96c527d25b972
SkyTorin <nick555@bk.ru>
2021-10-11 13:26:40 +0300

    Edited scripts 11.10.2021 13:26:00

--- MSSQL/sql_connect_02_(Win7).ps1
+++ MSSQL/sql_connect_02_(Win7).ps1
@@ -1,10 +1,10 @@
 # Создаём соединение
-$SqlServer = "vmtest";
-$SqlCatalog = "Clear2";
+$SqlServer = "Name_Server";
+$SqlCatalog = "Name_DB";
 $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
 $SqlConnection.ConnectionString = "Server=$SqlServer; Database=$SqlCatalog; Integrated Security=True"
 
-# Открываем соединен
+# Открываем соединение
 $SqlConnection.Open()
 
 # Выполнение c выводом результата
