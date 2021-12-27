52d9b608ab83c98375658e04cef6b3186d663cb7
nick <nick555@bk.ru>
2021-12-28 01:10:34 +0300

    Edited 27.12.2021 22:10:00

--- find_and_delete_files_older_than_x_days.ps1
+++ find_and_delete_files_older_than_x_days.ps1
@@ -1,1 +1,8 @@
-Get-ChildItem "C:\Site\*\logs\*\*.log" | Where CreationTime -lt (Get-Date).AddDays(-15) | Remove-Item -Force
+$path_from_logs = "C:\Sites\*\logs\*\*.log" 
+$older_days = "-15"
+
+# Поиск по дате создания и удаление файлов по маске старше X дней
+Get-ChildItem $path_from_logs | Where CreationTime -lt (Get-Date).AddDays($older_days) | Remove-Item -Force
+
+# Поиск по дате изменения и удаление файлов по маске старше X дней
+Get-ChildItem $path_from_logs | Where LastWriteTime -lt (Get-Date).AddDays($older_days) | Remove-Item -Force
